0e550b204730e4be176e65a78ce761be264e2417
Antonin-Winterstein <60510079+Antonin-Winterstein@users.noreply.github.com>
2022-01-17 20:42:32 +0100

    Fonction génération nombre aléatoire entre 0 et 9

--- js/randomNumberGenerator.js
+++ js/randomNumberGenerator.js
@@ -1,1 +1,8 @@
-function randomNumberGenerator() {}
+// Fonction pour retourner un chiffre aléatoire entre 0 et 9
+function randomNumberGenerator() {
+	document.getElementById("leftNumber").innerHTML = Math.floor(
+		Math.random() * 10
+	);
+}
+
+randomNumberGenerator();
