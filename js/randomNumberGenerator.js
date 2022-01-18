3da733198b6f74a68d36f933a47dd7cd2de2e1b6
Antonin-Winterstein <60510079+Antonin-Winterstein@users.noreply.github.com>
2022-01-18 16:07:44 +0100

    Actualisation des données avec les nombres anglais, fonctionnement du sélecteur de plages et affichage du nombre aléatoire entre celle-ci

--- js/randomNumberGenerator.js
+++ js/randomNumberGenerator.js
@@ -1,11 +1,10 @@
-// Fonction pour retourner un chiffre aléatoire entre 0 et 9
-function randomNumberGenerator() {
-	document.getElementById("leftNumber").innerHTML = Math.floor(
-		Math.random() * 10
+// Fonction pour retourner un chiffre aléatoire entre la valeur minimale et maximale récupérées en paramètres
+function randomNumberGenerator(minimumNumber, maximumNumber) {
+	return (
+		Math.floor(Math.random() * (maximumNumber - minimumNumber + 1)) +
+		minimumNumber
 	);
 }
-
-randomNumberGenerator();
 
 fetch("../data/data.json")
 	.then(function (response) {
@@ -13,7 +12,48 @@ fetch("../data/data.json")
 	})
 	.then(function (data) {
 		console.log(data);
-		console.log(data[Math.floor(Math.random() * 10)]);
+
+		let numberGenerator = document.getElementById("numberGenerator");
+		// Écouteur d'événement qui écoute l'événement submit
+		numberGenerator.addEventListener("submit", function (e) {
+			e.preventDefault();
+
+			// Les valeurs dans le formulaire
+			let minimumNumber =
+				document.getElementById("minimumNumber").valueAsNumber;
+			let maximumNumber =
+				document.getElementById("maximumNumber").valueAsNumber;
+
+			// Condition pour valider le formulaire
+			if (minimumNumber === "") {
+				alert("Veuillez saisir une valeur minimale.");
+			} else if (maximumNumber === "") {
+				alert("Veuillez saisir une valeur maximale.");
+			} else {
+				// Condition pour savoir si la valeur minimale est supérieure à la valeur maximale
+				if (minimumNumber > maximumNumber) {
+					// Si la condition est valide, on intervertit les valeurs dans les champs du formulaire
+					document.getElementById("minimumNumber").valueAsNumber =
+						maximumNumber;
+					document.getElementById("maximumNumber").valueAsNumber =
+						minimumNumber;
+					// Et on actualise les valeurs minimales et maximales
+					minimumNumber =
+						document.getElementById("minimumNumber").valueAsNumber;
+					maximumNumber =
+						document.getElementById("maximumNumber").valueAsNumber;
+				}
+
+				// Récupération du nombre aléatoire entre la valeur minimale et maximale voulue
+				let randomNumber = randomNumberGenerator(minimumNumber, maximumNumber);
+
+				// Affichage du nombre aléatoire
+				document.getElementById("leftNumber").innerHTML =
+					data[randomNumber].digital_numbers;
+				document.getElementById("rightNumber").innerHTML =
+					data[randomNumber].korean_numbers;
+			}
+		});
 	})
 	.catch(function (error) {
 		console.log(error);
